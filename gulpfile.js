ebc1213b7b1f1c972a523d49414da34e6ad1379b
Alan Languirand <alan@13protons.com>
2014-11-11 00:31:05 -0500

    Built basic site

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,20 @@ var gulp = require('gulp')
   , less = require('gulp-less')
   , path = require('path')
   , minifyCSS = require('gulp-minify-css')
+  , autoprefixer = require('gulp-autoprefixer')
   , serve = require('gulp-serve')
 
 
 gulp.task('styles', function() {
   gulp.src('./styles/*.less')
     .pipe(less())
-    //.pipe(minifyCSS({noAdvanced:true, keepSpecialComments: 0}))
+    .pipe(autoprefixer({
+          browsers: ['> 5%', 'last 5 versions', 'Firefox ESR', 'Opera 12.1', 'ios 6'],
+          cascade: true
+      }))
+    .pipe(minifyCSS({noAdvanced:true, keepSpecialComments: 0}))
     .pipe(gulp.dest('./styles/'));
+
 });
 
 gulp.task('serve', serve({
